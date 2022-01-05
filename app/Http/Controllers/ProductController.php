1358ce51929144170f1c5ab46dbbea2c6e7376ef
Abdo <raf7awe.overlord@gmail.com>
2022-01-05 15:04:27 +0200

    commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,11 @@ use App\Models\Product;
 
 class ProductController extends Controller
 {
+    /**
+     * Show specific product using route model. 
+     *
+     * @param Product $product
+     */
     public function show(Product $product)
     {
         return view('products.show', compact('product'));
